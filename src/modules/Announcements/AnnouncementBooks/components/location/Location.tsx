9ad53de0d87ea3c93f9e48de2595c953f50160bb
AzizbekGayratov <azizbekgairatov@gmail.com>
2024-09-29 18:36:33 +0500

    changes at AnnouncementBook and added a new page - Register

--- src/modules/Announcements/AnnouncementBooks/components/location/Location.tsx
+++ src/modules/Announcements/AnnouncementBooks/components/location/Location.tsx
@@ -1,12 +1,77 @@
+import api from "Services/Api";
 import { ComponentPropsType } from "modules/Announcements/types/Types";
+import { useEffect, useState } from "react";
+
+interface CityProps {
+  id: string;
+  name: {
+    en: string;
+    ru: string;
+    uz: string;
+  };
+}
+interface DistrictProps {
+  id: string;
+  name_json: string;
+}
 
 export default function Location({
   formData,
   setFormData,
 }: ComponentPropsType) {
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  // list
+  const [cities, setCities] = useState<CityProps[]>([]);
+  const [districtList, setDistrictList] = useState<DistrictProps[]>([]);
+
+  // selected
+  const [location, setLocation] = useState("");
+  const [district, setDistrict] = useState("");
+
+  // id
+  const [city_id, setCity_id] = useState("");
+  const [district_id, setDistrict_id] = useState("");
+
+  useEffect(() => {
+    const fetchList = async () => {
+      try {
+        const response = await api.get("/cities/list");
+        const data = response.data;
+        setCities(data.cities);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchList();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const city_id = cities.map((c) => {
+        let id;
+        if (location === c.name.en) {
+          id = c.id;
+          setCity_id(c.id);
+        }
+        return id;
+      });
+      try {
+        const response = await api.get(`/districts/list?id=${city_id}`);
+        const data = response.data;
+
+        setDistrictList(data.districts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [location]);
+
+  const handleInputChange = (id: string) => {
+    console.log({
+      city_id,
+      district_id: id,
+    });
+    setFormData({ ...formData, location: { city_id, district_id: id } });
   };
 
   return (
@@ -14,17 +79,35 @@ export default function Location({
       <h2 className="font-semibold text-[32px] text-primary">
         <label htmlFor="location">Manzilni kiriting*</label>
       </h2>
-
-      <input
-        type="text"
-        name="city_id"
-        id="location"
-        placeholder="Shahar yoki Pochta indeksi"
-        className="form_input mt-5 md:max-w-[48%]"
-        value={formData.location.city_id}
-        onChange={handleInputChange}
-        required
-      />
+      <div className="grid grid-cols-2 gap-5 mt-4">
+        <select
+          onChange={(e) => {
+            // e.preventDefault();
+            setLocation(e.target.value);
+          }}
+          className="form_input"
+        >
+          {cities.map((i, _) => (
+            <option key={_}>{i.name.en}</option>
+          ))}
+        </select>
+        {location.length !== 0 && (
+          <select
+            onChange={(e) => {
+              districtList.map((i, _) => {
+                if (JSON.parse(i.name_json).en === e.target.value) {
+                  handleInputChange(i.id);
+                }
+              });
+            }}
+            className="form_input"
+          >
+            {districtList.map((i, _) => (
+              <option key={_}>{JSON.parse(i.name_json).en}</option>
+            ))}
+          </select>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/modules/Profile/components/ProfileForm/components/FormTop/profileDelete/ProfileDelete.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import { Storage } from "../../../../../../../Services";
+import { MdDeleteOutline } from "react-icons/md";
+export default function ProfileDelete() {
+  const navigate = useNavigate();
+  const token = JSON.parse(Storage.get("token") as unknown as string);
+
+  const handleDeleteProfile = () => {
+    console.log(token);
+
+    const submitDelete = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_REACT_AUTH_URL}/auth/user/profile/delete`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token.access_token}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          Storage.clear();
+          navigate("/authorization/phone");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    submitDelete();
+  };
+
+  return (
+    <button
+      onClick={handleDeleteProfile}
+      className="flex items-center gap-4 sm:py-3 sm:px-6 p-2 rounded-full sm:rounded text-white text-[16px] leading-[19px] bg-primary"
+    >
+      <span className="hidden sm:block">Delete</span>
+      <MdDeleteOutline className="sm:w-6 sm:h-6 w-3 h-3" />
+    </button>
+  );
+}
